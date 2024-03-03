0de23cec39f1ba9dc5e6b1d2b7dbe4d908e1b698
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2024-03-03 20:22:54 +0100

    keep old one

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import chromium from '@sparticuz/chromium'
 
 async function scrape(url='https://www.nationalgeographic.com/'){
     try{
-        console.log('url page',url);
         puppeteerExtra.use(stealthPlugin())
 
         // Local Dev
@@ -29,7 +28,6 @@ async function scrape(url='https://www.nationalgeographic.com/'){
             const h2 = document.querySelector('h2')
             return h2 ? h2.innerText : null
         })
-        console.log('que es esttooooo', h2);
         // close the instance by closing all the pages
         const pages = await browser.pages()
         await Promise.all(pages.map(page => page.close()))
@@ -70,8 +68,57 @@ export const handler = async (event, context) => {
     }
 };
 
-// handler({
-//     body: JSON.stringify({
-//         url: 'https://www.google.com'
-//     })
-// })
+
+
+// Old Code
+
+// const puppeteer = require("puppeteer-core");j
+// const chromium = require("@sparticuz/chromium");
+
+// console.log("Loading function");
+// exports.handler = async (event, context) => {
+//   const browser = await puppeteer.launch({
+//     args: chromium.args,
+//     defaultViewport: chromium.defaultViewport,
+//     executablePath: await chromium.executablePath(
+//       process.env.AWS_EXECUTION_ENV
+//         ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
+//         : undefined
+//     ),
+//     headless: chromium.headless,
+//     ignoreHTTPSErrors: true,
+//   });
+//   const page = await browser.newPage();
+//   try {
+//     const urlToRead = event.webUrl;
+//     const domain = new URL(urlToRead).host;
+//     const outputFilePath = `${urlToRead.split(`${domain}/`)[1]}`;
+//     let now = new Date();
+//     now.setHours(48);
+//     const cookies = [
+//       {
+//         url: urlToRead,
+//         domain: domain,
+//         path: "/",
+//         expires: new Date().getTime(),
+//         "max-age": 60 * 60 * 24 * 2,
+//       },
+//     ];
+//     await page.setCookie(...cookies);
+//     await page.goto(urlToRead, {
+//       timeout: 60000,
+//       waitUntil: ["load", "networkidle0", "domcontentloaded"],
+//     });
+//     const html = await page.content();
+//     console.log('Html Page ',html);
+
+//     // db connection inside a try catch block
+
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     await page.close();
+//     await browser.close();
+//   }
+//   return;
+// };
