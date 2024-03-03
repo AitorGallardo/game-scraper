eaacd3d93af1de5547beb78aef741a7266ea1240
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2024-03-03 20:21:29 +0100

    updated version working on lambda

--- index.js
+++ index.js
@@ -1,50 +1,77 @@
-const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium");
+import puppeteerExtra from 'puppeteer-extra'
+import stealthPlugin from 'puppeteer-extra-plugin-stealth'
+import chromium from '@sparticuz/chromium'
 
-console.log("Loading function");
-exports.handler = async (event, context) => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(
-      process.env.AWS_EXECUTION_ENV
-        ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
-        : undefined
-    ),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-  const page = await browser.newPage();
-  try {
-    const urlToRead = event.webUrl;
-    const domain = new URL(urlToRead).host;
-    const outputFilePath = `${urlToRead.split(`${domain}/`)[1]}`;
-    let now = new Date();
-    now.setHours(48);
-    const cookies = [
-      {
-        url: urlToRead,
-        domain: domain,
-        path: "/",
-        expires: new Date().getTime(),
-        "max-age": 60 * 60 * 24 * 2,
-      },
-    ];
-    await page.setCookie(...cookies);
-    await page.goto(urlToRead, {
-      timeout: 60000,
-      waitUntil: ["load", "networkidle0", "domcontentloaded"],
-    });
-    const html = await page.content();
-    console.log('Html Page ',html);
+async function scrape(url='https://www.nationalgeographic.com/'){
+    try{
+        console.log('url page',url);
+        puppeteerExtra.use(stealthPlugin())
 
-    // db connection inside a try catch block
+        // Local Dev
+        // const browser = await puppeteerExtra.launch({
+        //     headless: true,
+        //     executablePath: '/usr/bin/google-chrome',
+        // })
 
-  } catch (error) {
-    console.log(error);
-  } finally {
-    await page.close();
-    await browser.close();
-  }
-  return;
+        const browser = await puppeteerExtra.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
+        });
+
+        const page = await browser.newPage()
+        await page.goto(url)
+
+        // get the h1 text
+        const h2 = await page.evaluate(() => {
+            const h2 = document.querySelector('h2')
+            return h2 ? h2.innerText : null
+        })
+        console.log('que es esttooooo', h2);
+        // close the instance by closing all the pages
+        const pages = await browser.pages()
+        await Promise.all(pages.map(page => page.close()))
+
+        await browser.close()
+        return h2
+
+    }catch(error){
+        console.log('error at scrape',error.message)
+    }
+}
+
+export const handler = async (event, context) => {
+    try{
+        const body = JSON.parse(event.body)
+        const {url} = body
+
+        const data = await scrape(url)
+        console.log('data',data);
+        
+        return { 
+            statusCode: 200, 
+            body: JSON.stringify({ 
+                message: 'Success',
+                requestedUrl: url,
+                data: data ?? 'No data'
+            })
+        }
+    } catch(error){
+        console.log('error at index.js',error.message)
+        return{
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Error at index.js',
+                error: error.message
+            })
+        }
+    }
 };
+
+// handler({
+//     body: JSON.stringify({
+//         url: 'https://www.google.com'
+//     })
+// })
