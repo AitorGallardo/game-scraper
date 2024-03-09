ba73081cb2ea55e9d3c338282a6f8e2b02341742
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2024-03-09 17:43:11 +0100

    retrieving correctly the games for every year with delay

--- index.js
+++ index.js
@@ -43,21 +43,6 @@ async function scrapeWithPuppeteer(url='https://www.nationalgeographic.com/'){
     }
 }
 
-async function scrapeWithCheerio(url='https://www.nationalgeographic.com/'){
-    axios.get(url)
-        .then(response => {
-            const $ = cheerio.load(response.data);
-
-            $('a.game-info-title').each((i, element) => {
-                const link = $(element).attr('href');
-                const title = $(element).text();
-                console.log(`Title: ${title}, Link: ${link}`);
-            });
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
 async function getGames(url='https://www.polygon.com/2020/12/14/22166004/best-games-2020-ps4-xbox-one-switch-pc-series-x'){
     axios.get(url)
         .then(response => {
@@ -71,57 +56,9 @@ async function getGames(url='https://www.polygon.com/2020/12/14/22166004/best-ga
             console.error(error);
         });
 }
-async function getGamesFromGoogle(url='https://www.polygon.com/2020/12/14/22166004/best-games-2020-ps4-xbox-one-switch-pc-series-x'){
-    axios.get(url)
-        .then(response => {
-            const $ = cheerio.load(response.data);
-            $('div[data-attrid="Munin"]').each((i, element) => {
-                const text = $(element).text();
-                console.log(text);
-            });
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-async function scrapeGamesGoogle(url='https://www.nationalgeographic.com/'){
-    try{
-        puppeteerExtra.use(stealthPlugin())
-
-        const browser = await puppeteerExtra.launch({
-            headless: true,
-            executablePath: '/usr/bin/google-chrome',
-        })
-
-        const page = await browser.newPage()
-        await page.goto(url)
-
-        const buttons = await page.evaluate(() => {
-            const buttons = Array.from(document.querySelectorAll('button'));
-            return buttons.find(button => button.innerText === 'Rechazar todo');
-        })
-
-        await page.evaluate(() => {
-            const buttons = Array.from(document.querySelectorAll('button'));
-            const actionButton = buttons.find(button => button.innerText === 'Rechazar todo');
-            if(actionButton) actionButton.click();
-        })
-
-        // close the instance by closing all the pages
-        const pages = await browser.pages()
-        await Promise.all(pages.map(page => page.close()))
-
-        await browser.close()
-        return buttons
-
-    }catch(error){
-        console.log('error at scrape',error.message)
-    }
-}
 
 
-async function scrapeGamesGoogleWithImages(url='https://www.nationalgeographic.com/',year=2020){
+async function getListOfGames(){
     try{
         puppeteerExtra.use(stealthPlugin())
 
@@ -129,21 +66,13 @@ async function scrapeGamesGoogleWithImages(url='https://www.nationalgeographic.c
             headless: false, // change this to false to see the browser UI
             executablePath: '/usr/bin/google-chrome',
         })
-
-        const page = await browser.newPage()
-        await page.goto(url)
-
-        await page.evaluate(() => {
-            const buttons = Array.from(document.querySelectorAll('button'));
-            const actionButton = buttons.find(button => button.innerText === 'Rechazar todo');
-            if(actionButton) actionButton.click();
-        })
-        const ariaLabels = await page.evaluate(() => {
-            const links = Array.from(document.querySelectorAll('a[aria-label]'));
-            return links.map(link => link.getAttribute('aria-label'));
-        });
-        console.log(pc.green('Year: '),pc.magenta(year));
-        console.log(ariaLabels, ariaLabels.length);
+        let year = 2022;
+        while(year <= 2023){
+            const url = `https://www.google.com/search?q=most+popular+games+${year}&sca_esv=1fcb60ee6ef69c6a&sxsrf=ACQVn08GKSjAxHE6n94NxfthkSvZ0AOVVA%3A1709679431068&ei=R6PnZfPjA6WI9u8PoL2TmAo&ved=0ahUKEwizo6yZnN6EAxUlhP0HHaDeBKMQ4dUDCBA&uact=5&oq=most+popular+games+2002&gs_lp=Egxnd3Mtd2l6LXNlcnAiF21vc3QgcG9wdWxhciBnYW1lcyAyMDAyMgUQABiABDIGEAAYFhgeMgsQABiABBiKBRiGAzILEAAYgAQYigUYhgNIzRZQmQpYkhJwAngBkAEAmAF4oAGyA6oBAzEuM7gBA8gBAPgBAZgCBaAC3wLCAgoQABhHGNYEGLADwgINEAAYgAQYigUYQxiwA8ICDhAAGOQCGNYEGLAD2AEBwgITEC4YgAQYigUYQxjIAxiwA9gBAsICFhAuGIAEGIoFGEMY1AIYyAMYsAPYAQLCAgoQIxiABBiKBRgnwgIKEAAYgAQYigUYQ5gDAIgGAZAGEroGBggBEAEYCboGBggCEAEYCJIHAzMuMqAHzRY&sclient=gws-wiz-serp`
+            await main(browser,url,year)
+            year++;
+        }
+        browser.close()
 
         // const pages = await browser.pages()
         // await Promise.all(pages.map(page => page.close()))
@@ -155,13 +84,37 @@ async function scrapeGamesGoogleWithImages(url='https://www.nationalgeographic.c
     }
 }
 
+const main = async(browser,url,year) => {
+    const page = await browser.newPage()
+    await page.goto(url)
+
+    await page.evaluate(() => {
+        const buttons = Array.from(document.querySelectorAll('button'));
+        const actionButton = buttons.find(button => button.innerText === 'Rechazar todo');
+        if(actionButton) actionButton.click();
+    })
+    const ariaLabels = await page.evaluate(() => {
+        const links = Array.from(document.querySelectorAll('a[aria-label]'));
+        return links.map(link => link.getAttribute('aria-label'));
+    });
+    console.log(pc.green('Year: '),pc.magenta(year));
+    console.log(ariaLabels, ariaLabels.length);
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            page.close()
+            resolve(ariaLabels);
+        }, 2500); 
+    });
+}
+
 
 export const handler = async (event, context) => {
     try{
         const body = JSON.parse(event.body)
-        const {url,year} = body
+        const {url} = body
 
-        const data = await scrapeGamesGoogleWithImages(url,year)
+        const data = await getListOfGames()
         console.log('data',data);
         
         return { 
@@ -183,7 +136,7 @@ export const handler = async (event, context) => {
         }
     }
 };
-let from = 2003;
+let from = 2023;
 const to = 2023;
 
 while(from <= to){
